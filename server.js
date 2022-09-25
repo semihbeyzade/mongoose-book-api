3fc72fc47a79908a087025bb7c057a351479cfdd
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-09-25 23:44:07 +0200

    initial commit with api

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3459;
+
+app.get('/', (req,res) => {
+    res.send(`<h1>Book API</h1>`)
+});
+
+app.listen(port, () => {
+    console.log(`listening on port: http://localhost:${port}`);
+})
